6e83709a62bd98786927c232a9ecebb804a3f03d
Hansel Perez <hanselperez1@gmail.com>
2017-12-31 00:57:16 -0500

    Added thrust to rocketship rigid body.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    Rigidbody rigidBody;
+
 	// Use this for initialization
 	void Start () {
-		
+        rigidBody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -19,15 +21,17 @@ public class Rocket : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.Space)) 
         {
-            print("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotating left");
-        } else if (Input.GetKey(KeyCode.D))
+            transform.Rotate(Vector3.forward);
+
+        } 
+        else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotating right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
